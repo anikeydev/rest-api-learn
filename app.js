3ff1a27fb152498ab7a4847d5cc26daa842105cf
anikey vladislav <dev.anikey.va@mail.ru>
2025-03-17 14:20:49 +0300

    finish learn

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const path = require('path')
+const { v4 } = require('uuid')
 
 const app = express()
 const PORT = 3000
+
+let CONTACTS = [
+  { id: v4(), name: 'Vladislav', value: '89998578427', marked: false },
+]
+
+app.use(express.json())
+
+app.get('/api/contacts', (req, res) => {
+  res.status(200).json(CONTACTS)
+})
+
+app.post('/api/contacts', (req, res) => {
+  const contact = { ...req.body, id: v4(), marked: false }
+  CONTACTS.push(contact)
+  res.status(201).json(contact)
+})
+
+app.delete('/api/contacts/:id', (req, res) => {
+  CONTACTS = CONTACTS.filter((c) => c.id !== req.params.id)
+  res.status(200).json({ message: 'Контакт был удален' })
+})
+
+app.put('/api/contacts/:id', (req, res) => {
+  const idx = CONTACTS.findIndex((c) => c.id === req.params.id)
+  CONTACTS[idx] = req.body
+  res.status(200).json(CONTACTS[idx])
+})
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
--- client/frontend.js
+++ client/frontend.js
@@ -16,18 +16,55 @@ createApp({
       contacts: [],
     }
   },
-  methods: {
-    createContact() {
-      const { ...contact } = this.form
-      this.contacts.push({ ...contact, id: new Date() })
-      this.form.name = this.form.value = ''
-      console.log(this.contacts)
-    },
-    markContact(id) {
-      console.log('mark')
-    },
-    deleteContact(id) {
-      console.log('delete')
+  computed: {
+    canCreate() {
+      return this.form.value.trim() && this.form.name.trim()
     },
   },
+  methods: {
+    async createContact() {
+      const { ...contact } = this.form
+      const newContact = await request('/api/contacts', 'POST', contact)
+      this.contacts.push(newContact)
+      this.form.name = this.form.value = ''
+    },
+    async markContact(id) {
+      const contact = this.contacts.find((c) => c.id === id)
+      const updated = await request(`/api/contacts/${id}`, 'PUT', {
+        ...contact,
+        marked: true,
+      })
+      contact.marked = updated.marked
+    },
+    async removeContact(id) {
+      await request(`/api/contacts/${id}`, 'DELETE')
+      this.contacts = this.contacts.filter((c) => c.id !== id)
+    },
+  },
+  async mounted() {
+    const data = await request('/api/contacts')
+    this.contacts = data
+  },
 }).mount('#app')
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {}
+    let body
+
+    if (data) {
+      headers['Content-Type'] = 'application/json'
+      body = JSON.stringify(data)
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body,
+    })
+
+    return await response.json()
+  } catch (e) {
+    console.warn('Error ' + e.message)
+  }
+}
